c2d898c2342ea0295c45053013f6cdbf3030e3d8
Ravindu Perera <ravinduramesh@users.noreply.github.com>
2019-11-10 08:28:40 +0530

    deleted unnccessary code block

--- api_maker.py
+++ api_maker.py
@@ -26,15 +26,4 @@ def makecalc():
 
 modelfile = 'models/final_prediction.pickle'
 model = p.load(open(modelfile, 'rb'))
-app.run(debug=True, host='0.0.0.0')
-
-import requests
-import json
-
-url = 'http://0.0.0.0:5000/api/'
-
-data = [[14.34, 1.68, 2.7, 25.0, 98.0, 2.8, 1.31, 0.53, 2.7, 13.0, 0.57, 1.96, 660.0]]
-j_data = json.dumps(data)
-headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-r = requests.post(url, data=j_data, headers=headers)
-print(r, r.text)
+app.run(debug=True, host='0.0.0.0') #gcp endpoint
